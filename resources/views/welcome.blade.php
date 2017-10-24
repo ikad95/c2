196060cd34fdf50d86b91e9f421ed70e0f59e45c
Moulik Adak <Moulik.adak@gmail.com>
2017-10-24 15:51:05 +0530

    Hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ikad
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/ikad95/c2">Project Repo</a>
                 </div>
             </div>
         </div>

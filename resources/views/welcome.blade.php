9a6d6b45ce4e6d73738aee196256193970ff4728
Moulik Adak <Moulik.adak@gmail.com>
2017-10-24 15:59:57 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    moulik
+                    ikad
                 </div>
 
                 <div class="links">
